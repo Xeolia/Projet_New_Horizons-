7c08d7290e9da85e7bbd4afd822773446294f8be
Cedric <cedricpasquierpro@gmail.com>
2020-05-14 11:18:27 +0200

    Add of multiple class on Modele folder.

---
+++ src/Modele/Discussion.java
@@ -0,0 +1,18 @@
+package Modele;
+
+import java.util.ArrayList;
+
+public abstract class  Discussion {
+
+    public int id;
+    ArrayList<Message> listeMessages;
+
+    public Discussion(){
+
+    }
+
+    public void envoyerMessage(){
+
+    }
+
+}
---
+++ src/Modele/DiscussionGroupe.java
@@ -0,0 +1,16 @@
+package Modele;
+
+import java.util.ArrayList;
+
+public class DiscussionGroupe extends Discussion{
+
+    public ArrayList<Utilisateur> listUtilisateurs;
+
+    public DiscussionGroupe(){
+
+    }
+
+    public void ajouterUtilisateur(){
+
+    }
+}
