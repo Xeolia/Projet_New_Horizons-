8ab30204ef7b4ff4727c1231a3d9702a3d32a022
Xeolia <alexia.serrierb@gmail.com>
2020-05-28 23:19:24 +0200

    Ajout d'un jscrollPan fonctionnel

--- src/main/java/Vue/PanelCentre.java
+++ src/main/java/Vue/PanelCentre.java
@@ -6,6 +6,7 @@ import java.awt.*;
 
 public class PanelCentre extends JPanel {
 
+    JPanel quatriemeDiscussion= new JPanel();
     JPanel premiereDiscussion = new JPanel();
     JPanel deuxiemeDiscussion = new JPanel();
     JPanel troisiemeDiscussion = new JPanel();
@@ -27,11 +28,13 @@ public class PanelCentre extends JPanel {
         premiereDiscussion.setBackground(Color.cyan);
         deuxiemeDiscussion.setBackground(Color.red);
         troisiemeDiscussion.setBackground(Color.gray);
+        quatriemeDiscussion.setBackground(Color.blue);
         this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        setLayout(new GridLayout(3, 1));
+        setLayout(new GridLayout(10, 1,30,50));
         this.add(premiereDiscussion);
         this.add(deuxiemeDiscussion);
         this.add(troisiemeDiscussion);
+        this.add(quatriemeDiscussion);
 
 
     }
--- src/main/java/Vue/PanelDiscussion.java
+++ src/main/java/Vue/PanelDiscussion.java
@@ -12,17 +12,23 @@ public class PanelDiscussion extends JPanel {
     public PanelDiscussion() {
         panelNord = new PanelNord();
         panelCentre = new PanelCentre();
-        this.setBorder(BorderFactory.createEmptyBorder(10,10,10,10)) ;
+        this.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         this.setLayout(new BorderLayout());
         add(panelCentre, BorderLayout.CENTER);
         add(panelNord, BorderLayout.NORTH);
         setVisible(true);
-
+        JScrollPane pane = new JScrollPane(panelCentre);
+        pane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        add(pane);
+        pane.setViewportView(panelCentre);
+        pane.revalidate();
 
     }
+
     public JPanel getPanelNord() {
         return panelNord;
     }
+
     public JPanel getPanelCentre() {
         return panelCentre;
     }
